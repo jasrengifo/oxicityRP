7011df2627907e2711469f999c164886fc79542c
stikernestea@gmail.com <stikernestea@gmail.com>
2021-01-23 00:48:44 +0000

    Revert "Probando 3"
    This reverts commit dc68de26238e87167ca07eaef2c677f70dee3550.

--- PruebasRP/server-data/resources/[esx]/esx_test/server.lua
+++ PruebasRP/server-data/resources/[esx]/esx_test/server.lua
@@ -19,7 +19,7 @@ RegisterCommand("givemoney", function(source, args)
 	if xPlayer.getGroup() == "admin" then
 		xPlayer.giveMoney(args[1])
 	else
-		print('No tienes permisos para usar este comando. ')
+		print('No tienes permisos para usar este comando.')
 	end
 
 end)
