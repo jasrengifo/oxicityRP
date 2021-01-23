af633e456fa4eee239cefa38c077706c74a1ec01
stikernestea@gmail.com <stikernestea@gmail.com>
2021-01-23 18:14:14 +0000

    teeest

--- PruebasRP/server-data/resources/[esx]/esx_test/server.lua
+++ PruebasRP/server-data/resources/[esx]/esx_test/server.lua
@@ -13,14 +13,7 @@ end)
  
 RegisterCommand("givemoney", function(source, args)
 
-	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-	local xPlayer = ESX.GetPlayerFromId(source)
 
-	if xPlayer.getGroup() == "admin" then
-		xPlayer.giveMoney(args[1])
-	else
-		print('No tienes permisos para usar este comando.. RensR')
-	end
 
 end)
  
