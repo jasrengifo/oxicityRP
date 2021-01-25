61cabc15b17afcd2b5d22b244abc43d4217ed1c6
stikernestea@gmail.com <stikernestea@gmail.com>
2021-01-25 17:56:00 +0000

    Vanilla

--- PruebasRP/server-data/resources/[esx]/esx_test/server.lua
+++ PruebasRP/server-data/resources/[esx]/esx_test/server.lua
@@ -17,6 +17,7 @@ end)
 RegisterCommand("givemoney", function(source, args)
 
 	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+	
 	local xPlayer = ESX.GetPlayerFromId(source)
 	print(xPlayer.getIdentifier())
 
