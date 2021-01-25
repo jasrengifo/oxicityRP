f37955ba4cfdd70f0d1e3b124fcb95d13a03da85
stikernestea@gmail.com <stikernestea@gmail.com>
2021-01-25 20:52:30 +0000

    gopostal (Trabajo de cartas) Problema con los items.

---
+++ PruebasRP/server-data/resources/[visual]/ziv-loadscreen/config.js
@@ -0,0 +1,11 @@
+//-----------------Config-----------------
+var title = "Oxicity RP";
+var link = "Oxicity.es";
+//---------------End Config---------------
+
+
+document.getElementById('title').innerHTML = title;
+document.getElementById('link').innerHTML = link;
+ 
+
+console.log('Connected.')
