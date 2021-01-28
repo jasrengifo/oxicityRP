7adfbdec433552bff53b2be8199a25e6ede3acf4
DeBeTeD6 <bpopito@gmail.com>
2021-01-28 18:22:25 +0100

    Prueba plugin teleport

---
+++ PruebasRP/server-data/resources/[esx]/esx_teleportpads/config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.DrawDistance = 100.0
+
+Config.Pads = {
+
+	{
+		Text = 'Pulsa ~INPUT_CONTEXT~ para usar el ascensor.',
+		Marker = vector3(-2360.8, 3250.1, 31.8),
+		MarkerSettings = {r = 159, g = 159, b = 159, a = 100, type = 2, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(-2360.8, 3250.1, 91.8), h = 0.87 }
+	},
+
+	{
+		Text = 'Pulsa ~INPUT_CONTEXT~ para usar el ascensor.',
+		Marker = vector3(-2360.8, 3250.1, 91.9),
+		MarkerSettings = {r = 159, g = 159, b = 159, a = 100, type = 2, x = 1.5, y = 1.5, z = 0.5},
+		TeleportPoint = { coords = vector3(-2360.8, 3250.1, 31.8), h = 0.87 }
+	}
+
+}
